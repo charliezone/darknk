d4839f6a4d104c75a6814cf4d139b137e86f9dc1
Carlos Rafael Gonzalez Perdomo <carlitos051186@gmail.com>
2020-05-10 23:26:43 -0400

    layout for 1366

--- wp-content/themes/Divi-child/js/app.js
+++ wp-content/themes/Divi-child/js/app.js
@@ -106,21 +106,6 @@
 
 jQuery(function ($) {
   $(document).ready(function () {
-    var cart = document.querySelector('.wpmenucart-display-standard .cartcontents');
-
-    if (cart) {
-      cart.textContent = cart.textContent.match(/\d+/g)[0];
-      cart.style.visibility = 'visible';
-    }
-
-    $(document.body).on('adding_to_cart added_to_cart removed_from_cart updated_wc_div ', function () {
-      cart = document.querySelector('.wpmenucart-display-standard .cartcontents');
-
-      if (cart) {
-        cart.textContent = cart.textContent.match(/\d+/g)[0];
-        cart.style.visibility = 'visible';
-      }
-    });
     var pagRight = document.querySelector('.blog-section .pagination .alignright a'),
         pagLeft = document.querySelector('.blog-section .pagination .alignleft a'),
         pagination = document.querySelector('.blog-section .pagination');
--- wp-content/themes/Divi-child/src/app.js
+++ wp-content/themes/Divi-child/src/app.js
@@ -1,19 +1,5 @@
 (jQuery)($ => {
     $(document).ready(() => {
-        let cart = document.querySelector('.wpmenucart-display-standard .cartcontents');
-        if(cart){
-            cart.textContent = cart.textContent.match(/\d+/g)[0];
-            cart.style.visibility = 'visible';
-        }
-
-        $( document.body ).on( 'adding_to_cart added_to_cart removed_from_cart updated_wc_div ', () => {
-            cart = document.querySelector('.wpmenucart-display-standard .cartcontents');
-            if(cart){
-                cart.textContent = cart.textContent.match(/\d+/g)[0];
-                cart.style.visibility = 'visible';
-            }
-        });
-
         const pagRight = document.querySelector('.blog-section .pagination .alignright a'),
               pagLeft = document.querySelector('.blog-section .pagination .alignleft a'),
               pagination = document.querySelector('.blog-section .pagination');
